618aae223c9e82cea38e40ed76e8760e68bf1a11
ridarf <getridahim@gmail.com>
2021-03-19 23:12:36 -0600

    Implemented rotation algorithm

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,14 @@ function shiftSerialNumber(
     if (passes <= 0 || !serialNumber) 
         return serialNumber;
 
-    const nybbles: string[] = [...serialNumber];
+    const head = serialNumber.substr(0, serialNumber.length - 2);
+    const tail = serialNumber.substr(-2);
+
+    const nybbles: string[] = [...head];
     const rotated: string[] = nybbles.map(nybble => {
         const nudged = (parseInt(nybble, 16) + passes) % 16;
         return nudged.toString(16);
     })
 
-    return rotated.join("");
+    return 
 }
