17b442e97a0c1eaccdefd0f25daddb741165a211
Zep Molnar <Getridahim@gmail.com>
2021-03-23 16:58:03 -0600

    added initial client-side

--- electron-app/main.js
+++ electron-app/main.js
@@ -1,21 +1,69 @@
+const path = require('path');
+
 const { app, BrowserWindow, ipcMain } = require('electron');
 
 const { io } = require('socket.io-client');
-const socket = io("http://raspberrypi.localdomain/");
 
-socket.on('connection', () => {
-
-});
+let socket;
 
 const createWindow = () => {
   const window = new BrowserWindow({
     height: 420,
-    width: 420,
+    width: 480,
+    show: false,
     resizable: false,
-    autoHideMenuBar: true
-  })
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: true,
+      preload: path.join(__dirname, "preload.js"),
+      contextIsolation: false
+    },
+    alwaysOnTop: true
+  });
 
+  window.webContents.on('did-finish-load', () => {
+    window.show();
+
+    if (socket) {
+      if (socket.connected) handleConnect();
+    } else {
+      socket = io("http://raspberrypi.localdomain/");
+      socket.on('connect', handleConnect);
+      socket.on('disconnect', handleDisconnect);
+      socket.on('reconnect_error', handleFailedReconnect);
+    }
+  })
+  
   window.loadURL("http://localhost:3000/");
+
+  function handleConnect() {
+    console.log("Connected");
+    window.webContents.send(
+      "change-status",
+      "check",
+      "Connection Successful",
+      "You are connected to the server, all scans will be typed out through this system."
+    );
+  }
+
+  function handleDisconnect() {
+    window.webContents.send(
+      "change-status",
+      "info",
+      "Reconnecting...",
+      "Connection to server failed, make sure the server is on while reconnection attempts are made."
+    );
+  }
+
+  function handleFailedReconnect() {
+    window.webContents.send(
+      "change-status",
+      "danger",
+      "Connection Failed",
+      "Connection to server failed, reconnection attempts exceeded. Ensure the server is online, and then click to reconnect."
+    )
+  }
+
 }
 
 app.on('ready', createWindow);
--- electron-app/src/App.js
+++ electron-app/src/App.js
@@ -3,30 +3,51 @@ import warningIcon from './img/warning.svg';
 import infoIcon from './img/info.svg';
 import checkIcon from './img/check.svg';
 
+import { useState, useEffect } from 'react';
+
 import './App.css';
 
+const { ipcRenderer } = window.require("electron");
+
 function App() {
-  const statuses = ["danger", "warning", "info", "check"];
-  let status = "danger";
+  const [message, setMessage] = useState("Connecting");
+  const [description, setDescription] = useState("Connecting to NFC Server");
+  const [status, setStatus] = useState("info");
+
+  useEffect(() => {
+    ipcRenderer.on("change-status", (event, newStatus, newMessage, newDescription) => {
+      setStatus(newStatus)
+      setMessage(newMessage);
+      setDescription(newDescription);
+    })
+  }, [])
 
   return (
     <div className={"App " + status}>
-      <div className="App-notification-container">
-        {status === "danger" &&
-          <img className="App-notification-icon" src={dangerIcon} alt="" srcset=""/>
-        }
-        {status === "warning" &&
-          <img className="App-notification-icon" src={warningIcon} alt="" srcset=""/>
-        }
-        {status === "info" &&
-          <img className="App-notification-icon" src={infoIcon} alt="" srcset=""/>
-        }
-        {status === "check" &&
-          <img className="App-notification-icon" src={checkIcon} alt="" srcset=""/>
-        }
-        <div className="App-bulb App-bulb-s"></div>
-        <div className="App-notification"></div>
-        <div className="App-bulb App-bulb-l"></div>
+      <div className="App-root">
+        <div className="App-notification-container">
+          {status === "danger" &&
+            <img className="App-notification-icon" src={dangerIcon} alt="" srcSet=""/>
+          }
+          {status === "warning" &&
+            <img className="App-notification-icon" src={warningIcon} alt="" srcSet=""/>
+          }
+          {status === "info" &&
+            <img className="App-notification-icon" src={infoIcon} alt="" srcSet=""/>
+          }
+          {status === "check" &&
+            <img className="App-notification-icon" src={checkIcon} alt="" srcSet=""/>
+          }
+          <div className="App-bulb App-bulb-s"></div>
+          <div className="App-notification"></div>
+          <div className="App-bulb App-bulb-l"></div>
+        </div>
+        <div className="App-notification-message">{message}</div>
+        <div className="App-notification-description">{description}</div>
+      </div>
+      <div className="App-buttons">
+          <button className="App-button">Action</button>
+          <button className="App-button App-button-highlight">Highlight Action</button>
       </div>
     </div>
   );
