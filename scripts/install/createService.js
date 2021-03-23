d5241d6bd639c5904ef222581192bec75d59bf1c
ridarf <getridahim@gmail.com>
2021-03-22 19:21:58 -0600

    edited service creation directory to accept scripts

--- scripts/install/createService.js
+++ scripts/install/createService.js
@@ -1,11 +1,9 @@
-const fs = require('fs');
-
 const service = `[Unit]
 Description=ReNFC Serial Remapping
 After=network.target
 
 [Service]
-WorkingDirectory=${__dirname}
+WorkingDirectory=${process.cwd()}
 ExecStart=node build/index.js
 Restart=on-failure
 Environment=PORT=80
@@ -13,4 +11,4 @@ Environment=PORT=80
 [Install]
 WantedBy=multi-user.target`
 
-fs.writeFileSync('/etc/systemd/system/nfc.service', service, 'utf8');
+require('fs').writeFileSync('/etc/systemd/system/nfc.service', service, 'utf8');
