5148416009a69811df75c7f162d113fabe86fee7
ridarf <Getridahim@gmail.com>
2021-03-24 10:50:08 -0600

    re-shortened interval & timeout

--- src/modules/WebSocket.ts
+++ src/modules/WebSocket.ts
@@ -9,8 +9,8 @@ export class WebSocket extends EventEmitter {
     app = express();
     server: http.Server = createServer(this.app);
     io: Server = new Server(this.server, {
-        pingInterval: 5000,
-        pingTimeout: 10000
+        pingInterval: 2500,
+        pingTimeout: 7500
     });
 
     constructor() {
@@ -18,7 +18,7 @@ export class WebSocket extends EventEmitter {
     }
     
     startServer() {
-        this.server.listen(5001, () => this.emit("ready"))
+        this.server.listen(80, () => this.emit("ready"))
     }
 
 }
